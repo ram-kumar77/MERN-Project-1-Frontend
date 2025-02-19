211756ac25c79dd6c382af91f069288ffda0c361
Ram kumar <ramkumarsundar07@gmail.com>
2025-02-19 12:18:43 +0530

    Updated Render URL for API calling

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-project-1-backend-dlx4.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust based on your backend port
+  baseURL: 'https://mern-project-1-backend-dlx4.onrender.com', // Adjust based on your backend port
   headers: {
     'Content-Type': 'application/json',
   }
